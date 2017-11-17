9f8eb67f7ea548da8bf9403abf03b929cbc77a93
Evis Bregu <evis.bregu@gmail.com>
2017-11-17 11:00:05 +0100

    Simple errors

---
+++ errors.go
@@ -0,0 +1,21 @@
+package main
+
+import	(
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 2 {
+		os.Exit(1)
+	}
+
+	n, err := strconv.Atoi(os.Args[1])
+
+	if err != nil {
+		fmt.Println("Not a valid number")
+	} else {
+		fmt.Println(n)
+	}
+}
